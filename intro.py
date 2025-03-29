13fa718f8e8cb5122fda392858bab4fc8e909454
Rafael Jamur <rjamur@gmail.com>
2025-03-29 10:51:37 -0300

    Alien just clicked

---
+++ intro.py
@@ -0,0 +1,26 @@
+alien = Actor('alien')
+alien.pos = 100, 56
+
+WIDTH = 500
+HEIGHT = alien.height + 20
+alien.topright = 0, 10
+
+def draw():
+	screen.clear()
+	alien.draw()
+
+def update():
+	alien.left += 2
+	if alien.left > WIDTH:
+		alien.right = 0
+
+def on_mouse_down(pos):
+	if alien.collidepoint(pos):
+		set_alien_hurt()
+
+def set_alien_hurt():
+	alien.image = 'alien_hurt'
+	sounds.eep.play()
+
+def set_alien_normal():
+	alien.image = 'alien'
